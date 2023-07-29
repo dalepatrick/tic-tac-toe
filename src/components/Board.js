7f70919c81677e7f5c20a269ca07a916ab2b3e2f
dalepatrick <dalepatrick005@gmail.com>
2023-07-30 02:06:11 +0800

    added some comments on the code and import the box

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { Box } from "./Box"; // Importing the Box component
+import { Box } from "./Box";
 import "./Board.css";
 
-// Board component renders the Tic-Tac-Toe game board
 export const Board = ({ board, onClick }) => {
   return (
     <div className="board">
       {/* Map over each value in the 'board' array and render a Box component for each */}
-      {board.map((value, idx) => {
-        // The 'onClick' function is conditionally set to be called only if the box is empty (value === null)
-        return <Box value={value} onClick={() => value === null && onClick(idx)} />;
-      })}
+      {board.map((value, idx) => (
+        // Add a unique 'key' prop to the Box component using the index (idx) of the element
+        <Box key={idx} value={value} onClick={() => value === null && onClick(idx)} />
+      ))}
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
 import React from "react";
-
 import "./Header.css";
 
-
+//  Header component renders the header title for the Tic-Tac-Toe game
 export const Header = () => {
-    return (
-        <div className="header">Tic-Tac-Toe</div>
-    )
+  return (
+    <div className="header">Tic-Tac-Toe</div>
+  );
 };
